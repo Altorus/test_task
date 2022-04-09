055d50c8a3a07b34e9c8022487a280f8b04c85cb
Festanny <60522731+Festanny@users.noreply.github.com>
2022-04-09 15:12:03 +0400

    upd

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 $('.js-tab-trigger').click(function() {
     var id = $(this).attr('data-tab'),
-        content = $('.js-tab-content[data-tab="'+ id +'"]');
-    
+        content = $('.js-tab-content[data-tab="' + id + '"]');
+
     $('.js-tab-trigger.active').removeClass('active'); // 1
     $(this).addClass('active'); // 2
-    
+
     $('.js-tab-content.active').removeClass('active'); // 3
     content.addClass('active'); // 4
- });
+
+});
